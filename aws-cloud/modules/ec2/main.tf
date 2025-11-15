7c7e58ae9230e076e75190da0f1d51331406953b
donhasmo <alhassani.y1k@gmail.com>
2025-11-15 01:33:23 +0100

    updates

--- aws-cloud/modules/ec2/main.tf
+++ aws-cloud/modules/ec2/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "my_ec2" {
   ami                    = var.ami # Ubuntu 20.04 AMI (update for your region)
-  instance_type          = "t3.micro"
+  instance_type          = "t3.small"
   key_name               = aws_key_pair.dev_key.key_name             # Your pem key name (without .pem)
   subnet_id              = var.react_pubsub_id
   vpc_security_group_ids = var.pub_sg_id
